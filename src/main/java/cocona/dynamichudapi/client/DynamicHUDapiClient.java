d9b4171fc9b7c94baa61b268f1e5f2348b54fa14
cocona20xx <pexium128@gmail.com>
2021-01-30 18:57:54 -0500

    Initial commit

---
+++ src/main/java/cocona/dynamichudapi/client/DynamicHUDapiClient.java
@@ -0,0 +1,13 @@
+package cocona.dynamichudapi.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class DynamicHUDapiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
